d6512ea5b62f9a0d132c06335926dc9e31c07a5d
PratikAgrawal02 <pratik.a21@iiits.in>
2022-11-12 09:18:33 +0530

    first commit

---
+++ app/src/main/java/com/pratik/medex/appointment/AppointmentModel.kt
@@ -0,0 +1,3 @@
+package com.pratik.medex.appointment
+
+class AppointmentModel (var filename: String? = null, var fileurl:String? = null)
---
+++ app/src/main/java/com/pratik/medex/appointment/BookingDoneActivity.kt
@@ -0,0 +1,25 @@
+package com.pratik.medex.appointment
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.pratik.medex.HomeActivity
+import com.pratik.medex.R
+
+class BookingDoneActivity : AppCompatActivity() {
+
+    lateinit var handler: Handler
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_booking_done)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1500)
+    }
+}
